3f6fe03832f4d9d9ed6d09d685136cb14ae48d0c
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-10-19 12:37:08 -0400

    added most of controller for contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,46 @@
 class ContactsController < ApplicationController
+
+  def index
+    @household = Household.find(params[:@household_id]) 
+    @contacts = Contact.where(@household_id: @household.id) 
+
+    render json: @contacts, include: :@household_id, status: :ok
+  end
+
+  def show
+    render json: @contact
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      #is this the right contact or os the the contact with ID
+      render json: @contact, status: :created, location: @household_id
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @contact.update(contact_params)
+      render json: @contact
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @contact.destroy
+  end
+
+  private
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:name)
+  end
+
 end
