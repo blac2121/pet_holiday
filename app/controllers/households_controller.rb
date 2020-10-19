32ca501a6820e583e917911a198eaab1a27dfca7
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-10-19 12:20:02 -0400

    controllers household done through create

--- app/controllers/households_controller.rb
+++ app/controllers/households_controller.rb
@@ -1,2 +1,45 @@
 class HouseholdsController < ApplicationController
+
+  def index
+    @households = Household.all 
+
+    render json: @households
+  end
+
+  def show
+    render json: @household, include: [:pets :contacts]
+  end
+
+  def create
+    @household = Household.new(household_params)
+
+    if @household.save
+      render json: @household, status: :created, location: @household
+    else
+      render json: @household.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @dojo = Dojo.find(params[:id])
+    @dojo.update(params)
+
+    render json: @dojo
+  end
+
+  def destroy
+    @dojo = Dojo.find(params[:id])
+
+    @dojo.destroy
+  end
+
+  private
+  def set_household
+    @household = Household.find(params[:id])
+  end
+
+  def household_params
+    params.require(:household).permit(:name)
+  end
+
 end
