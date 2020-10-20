cb8862faff11487f3417978c3c25f83aea1b4f1f
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-10-20 16:45:18 -0400

    light styling on nav

--- client/src/components/shared/Nav.jsx
+++ client/src/components/shared/Nav.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  display: flex;
+  background-color: #F9F9FA;
+  box-shadow: 0 10px 25px -5px rgba(0, 0, 0, 0.3);
+  background-color: #F9F9FA;
+`
+
+const Title = styled.h1`
+  color: #DAAB5E;
+  font-size: 48px;
+  margin: 40px 50px 50px;
+`
 
 const Nav = () => {  
   return (
-    <div>
+    <MainContainer>
       <Link to='/'>
-        <h1>Pet Holiday</h1>
+        <Title>Pet Holiday</Title>
       </Link>      
-    </div>
+    </MainContainer>
   );
 };
 
