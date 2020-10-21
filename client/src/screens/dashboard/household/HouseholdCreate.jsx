52426d098683a132a55a29cc636b12456c51e97b
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-10-21 11:03:53 -0400

    create household form complete and working

--- client/src/screens/dashboard/household/HouseholdCreate.jsx
+++ client/src/screens/dashboard/household/HouseholdCreate.jsx
@@ -3,14 +3,26 @@ import Layout from '../../../components/shared/Layout';
 
  const CreateHousehold = (props) => {
   const [household, setHousehold] = useState({
-    name: ''
+    name: '',
+    street: '',
+    city: '',
+    state: '',
+    zip_code: '',
+    temp_low: '',
+    temp_high: '',
+    wifi_password: '',
+    wifi_username: '',
+    notes: ''
   })
    
   const { handleHouseholdCreate } = props;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setHousehold({ [name]: value})
+    setHousehold({
+      ...household,
+      [name]: value
+    })
   }
    
   const handleSubmit = (e) => {
@@ -30,6 +42,62 @@ import Layout from '../../../components/shared/Layout';
           autoFocus
           onChange={handleChange}
         />
+        <label>Street</label>
+        <input
+          type="text"
+          name="street"
+          value={household.street}
+          onChange={handleChange}
+        />
+        <label>City</label>
+        <input
+          value={household.city}
+          name='city'
+          onChange={handleChange}
+        />
+        <label>State</label>
+        <input
+          value={household.state}
+          name='state'
+          onChange={handleChange}
+        />
+        <label>Zip Code</label>
+        <input
+          value={household.zip_code}
+          name='zip_code'
+          onChange={handleChange}
+        />
+        <label>Temperature</label>
+        <input
+          placeholder="low"
+          value={household.temp_low}
+          name='temp_low'
+          onChange={handleChange}
+        />
+        <input
+          placeholder="high"
+          value={household.temp_high}
+          name='temp_high'
+          onChange={handleChange}
+        />
+        <label>Wifi Username</label>
+        <input
+          value={household.wifi_username}
+          name='wifi_username'
+          onChange={handleChange}
+        />
+        <label>Wifi Password</label>
+        <input
+          value={household.wifi_password}
+          name='wifi_password'
+          onChange={handleChange}
+        />
+        <label>Notes</label>
+        <input
+          value={household.notes}
+          name='notes'
+          onChange={handleChange}
+        />        
         <button type='submit'>Submit</button>
       </form>
     </Layout>
