28e6dd5eaf111525b8da846981e5fd8405334a9d
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-10-22 15:39:08 -0400

    fixed api and added redirect

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,12 +1,21 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production'
-  ? 'https://pet-holiday-api.herokuapp.com/'
-  : 'http://localhost:3000' 
+let apiUrl
+
+const apiUrls = {
+  production: 'https://pet-holiday-api.herokuapp.com/',
+  development: 'http://localhost:3000'
+}
+
+if (window.location.hostname === 'localhost') {
+  apiUrl = apiUrls.development
+} else {
+  apiUrl = apiUrls.production
+}
 
 
 const api = axios.create({
-  baseUrl: baseUrl
+  baseURL: apiUrl
 })
 
 export default api;
