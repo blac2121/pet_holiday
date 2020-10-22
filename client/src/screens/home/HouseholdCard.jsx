8c1ec3ebb62188a6a8e4a78f354bf4d22d3a8ac8
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-10-22 15:15:34 -0400

    added styling

--- client/src/screens/home/HouseholdCard.jsx
+++ client/src/screens/home/HouseholdCard.jsx
@@ -8,7 +8,8 @@ import EditButton from '../../components/EditButton';
 import styled from 'styled-components';
 
 const HouseholdContainer = styled.div`
-  border: 3px solid #DAAB5E;
+  border: 1px solid #DFDFDF;
+  box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.3);
   border-radius: 3px;
   width: 60vw;
   margin: 30px;
@@ -17,6 +18,7 @@ const HouseholdContainer = styled.div`
 
 const HouseholdTitle = styled.h4`
   font-size: 24px;
+  color: #23309E;
 `
 
 const HouseholdCard = (props) => { 
