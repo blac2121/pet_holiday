9e6a0d82218217feeef7d88cff2708caf6c66475
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-10-26 09:45:14 -0400

    updated code

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,6 +1,6 @@
 class Household < ApplicationRecord
-  has_many :contacts, :dependent => :destroy
-  has_many :pets, :dependent => :destroy
+  has_many :contacts, dependent: :destroy
+  has_many :pets, dependent: :destroy
 
   validates :name, presence: true
 end
